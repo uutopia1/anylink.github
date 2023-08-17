97b668126020675d1faba499017679e243157cec
tmax <tmax@DESKTOP-PMAJ8J4>
2023-08-17 11:00:00 +0900

    add messages

---
+++ 한화손해보험/.src/hwgi/o28/mem/txGrp_1000/tx_220/MSG.java
@@ -0,0 +1,218 @@
+/*
+ * 작성된 날짜: Aug 17, 2023
+ * Copyright (c) 2023 TmaxSoft co., Ltd. All rights reserved.
+ */
+package hwgi.o28.mem.txGrp_1000.tx_220;
+
+import com.tmax.promapper.engine.base.ProMapperDataObject;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.List;
+import proframe.tool.record.common.*;
+import com.tmax.promapper.engine.base.ResourceMeta;
+import com.tmax.promapper.engine.util.StringUtil;
+
+
+/**
+ * @file              hwgi.o28.mem.txGrp_1000.tx_220.MSG.java
+ * @filetype          java source file
+ * @brief            
+ * @version           1.0
+ * @history
+ * 성 명              일 자                     근 거 자 료                      변 경 내 용
+ * -----------        --------                  --------------                   --------------------------    
+ * AnyLink :       Aug 17, 2023                AnyLink 개발 :                신규 작성
+ * 
+ */
+
+public class MSG extends ProMapperDataObject implements ResourceMeta {
+    private static final long serialVersionUID= 1L;
+    public MSG() {
+            super();
+    }
+    private boolean _setFlag = false;
+    private String data = null;
+    
+    public String getData() {
+        return data;
+    }
+
+    public String getData(boolean _masked) {
+        return StringUtil.nullableStringValueOf(getData());
+    }
+                      
+    public void setData(String data) {
+        this.data = data;
+        _setFlag = true;
+    }
+
+    public void setData(String data, boolean _masked) {
+        setData(data);
+    }
+
+
+            
+    private String data1 = null;
+    
+    public String getData1() {
+        return data1;
+    }
+
+    public String getData1(boolean _masked) {
+        return StringUtil.nullableStringValueOf(getData1());
+    }
+                      
+    public void setData1(String data1) {
+        this.data1 = data1;
+        _setFlag = true;
+    }
+
+    public void setData1(String data1, boolean _masked) {
+        setData1(data1);
+    }
+
+
+            
+    public Object clone() {
+        MSG copyObj = new MSG();
+        clone(copyObj);
+        return copyObj;
+    }
+    public void clone(proframe.dto.DataObject __mSG) {
+        MSG _mSG = (MSG) __mSG;
+        _mSG.setData(data);
+        _mSG.setData1(data1);
+    }
+    public String toString() {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append(super.toString());
+
+            buffer.append('\n');
+            toString_0(buffer);
+        
+        return buffer.toString();
+    }
+    
+    private void toString_0(StringBuilder buffer) {
+        int _size = 0; // field가 array인 경우 array size를 저장하는 변수
+            buffer.append("data : ").append(data).append("\n");
+            buffer.append("data1 : ").append(data1).append("\n");
+    }    
+    public String toString(int limit) {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append(super.toString());
+        int _size = 0; // field가 array인 경우 array size를 저장하는 변수
+        
+            buffer.append('\n');
+            toString_0(buffer, limit);
+        
+        return buffer.toString();
+    }
+    
+    private void toString_0(StringBuilder buffer, int limit) {
+        int _size = 0; // field가 array인 경우 array size를 저장하는 변수
+        if(buffer.length() < limit) {
+            buffer.append("data : ").append(data).append("\n");
+        }
+        if(buffer.length() < limit) {
+            buffer.append("data1 : ").append(data1).append("\n");
+        }
+    }    
+    private static final Map fieldPropertyMap = null;
+    
+/*    static {
+        parentfieldPropertyMap = ProMapperDataObject.getFieldPropertyMapByStatic();
+        fieldPropertyMap = new java.util.LinkedHashMap( parentfieldPropertyMap.size() + 2);
+        fieldPropertyMap.putAll( parentfieldPropertyMap );
+
+        fieldPropertyMap.put("data"
+                            , FieldPropertyFactory.getFieldProperty( FieldProperty.TYPE_ABSTRACT_STRING, -1, null, null));
+        fieldPropertyMap.put("data1"
+                            , FieldPropertyFactory.getFieldProperty( FieldProperty.TYPE_ABSTRACT_STRING, -1, null, null));
+    }*/
+    
+    public Map getFieldPropertyMap() {
+        return fieldPropertyMap;
+    }
+    
+    public static Map getFieldPropertyMapByStatic() {
+        return fieldPropertyMap;
+    }
+    private static final List<String> fieldNameList = new ArrayList<String>(2);
+    
+    static {        
+        fieldNameList.add("data");
+        fieldNameList.add("data1");
+    }
+    
+    public List<String> getFieldNameList(){
+        return fieldNameList;
+    }
+    
+    public boolean getSetFlag(){
+        return _setFlag;
+    }
+
+    public Object get(String fieldName) {
+        int hashCode = fieldName.hashCode();
+        
+        switch(hashCode) {
+            case 3076010 : return getData();
+            case 95356359 : return getData1();
+            default : return null;
+        }
+    }
+    public void set(String fieldName, Object arg) {
+        int hashCode = fieldName.hashCode();
+        
+        switch(hashCode) {
+            case 3076010 : setData((String)arg);break;
+            case 95356359 : setData1((String)arg);break;
+            default : return;
+        }
+    }
+    public boolean isEmptyDataObjectMethod(){ 
+        if (data != null) return false;
+        if (data1 != null) return false;
+
+        return true;
+    }
+
+    /**
+     * @param input 
+     *  values of input variable are 
+     *                                         physicalname
+     *                                        versionno
+     *                                        logicalname
+     *                                        resourceid
+     *                                   resourceType
+     *                                   resourcePath
+     *                                   resourceGroup
+     * @return 
+     */
+    public String getMetaData(String input) {
+
+        if (input.equalsIgnoreCase(META_PHYSICAL_NAME)) {
+            return "MSG";
+        } else if (input.equalsIgnoreCase(META_VERSION_NO)){
+            return null;
+        } else if (input.equalsIgnoreCase(META_LOGICAL_NAME)){
+            return "MSG";
+        } else if (input.equalsIgnoreCase(META_RESOURCE_ID)){
+            return "hwgi.o28.mem.txGrp_1000.tx_220:MSG.umsg";
+        } else if (input.equalsIgnoreCase(META_RESOURCE_TYPE)){
+            return "STRUCTURE";
+        } else if (input.equalsIgnoreCase(META_RESOURCE_PATH)){
+            return "hwgi.o28.mem.txGrp_1000.tx_220";
+        } else if (input.equalsIgnoreCase(META_RESOURCE_GROUP)){
+            return "";
+        } else {
+            String msg = "Expecting one of " + META_PHYSICAL_NAME + ", "
+                    + META_VERSION_NO + ", " + META_LOGICAL_NAME + ", "
+                    + META_RESOURCE_ID + ", " + META_RESOURCE_TYPE + ", "
+                    + META_RESOURCE_PATH + ", " + META_RESOURCE_GROUP;
+            throw new IllegalArgumentException(msg);
+        }
+    }
+}
---
+++ 한화손해보험/.src/hwgi/o28/mem/txGrp_1000/tx_220/MSGFixedLength.java
@@ -0,0 +1,206 @@
+/*
+ * 작성된 날짜: Aug 17, 2023
+ * Copyright (c) 2023 TmaxSoft co., Ltd. All rights reserved.
+ */
+package hwgi.o28.mem.txGrp_1000.tx_220;
+
+import com.tmax.promapper.engine.base.ProMapperDataObject;
+import proframe.dto.DataObject;
+import java.util.logging.Level;
+import com.tmax.anylink.logging.Logger;
+import com.tmax.promapper.engine.base.MessageFieldProperty;
+import com.tmax.promapper.engine.base.MessageFieldType;
+import com.tmax.promapper.engine.base.ResourceMeta;
+import com.tmax.promapper.engine.base.Message;
+import com.tmax.promapper.engine.util.Constants;
+import com.tmax.promapper.engine.util.FixedLengthMarshalField;
+import com.tmax.promapper.engine.util.FixedLengthMarshalFieldV2;
+import com.tmax.promapper.engine.util.FixedLengthUnmarshalField;
+
+import com.tmax.promapper.engine.base.FixedLengthMessage;
+
+/**
+ * @file              hwgi.o28.mem.txGrp_1000.tx_220.MSGFixedLength.java
+ * @filetype          java source file
+ * @brief            
+ * @version           1.0
+ * @template-version  2014-11-12
+ * @history
+ * 성 명              일 자                     근 거 자 료                      변 경 내 용
+ * -----------        --------                  --------------                   --------------------------    
+ * AnyLink :       Aug 17, 2023                AnyLink 개발 :                신규 작성
+ * 
+ */
+
+
+
+public class MSGFixedLength extends com.tmax.promapper.engine.base.FixedLengthMessage implements ResourceMeta {
+
+    public MSGFixedLength() {
+        super();
+    }
+
+    public MSGFixedLength(int conversionType) {
+        super(conversionType);
+    }
+
+    public MSGFixedLength(String charsetName) {
+        super(charsetName);
+    }
+    
+    public void setMasked(boolean _masked) {
+        super.setMasked(_masked);
+    }
+
+    /* marshal method */
+    public byte [] marshalObject(Object obj) throws Exception {
+        FixedLengthMarshalFieldV2 flFld = new FixedLengthMarshalFieldV2();
+        hwgi.o28.mem.txGrp_1000.tx_220.MSG __root = (hwgi.o28.mem.txGrp_1000.tx_220.MSG)obj;
+
+        if(__root == null) {
+            marshallNullObject(flFld);
+        } else {
+            
+            /** data1 */
+            flFld.setStringField(this, __root.getData1(this.masked), 10, Constants.ALIGN_NON, " ", 0, true, false, Constants.ENCODE_CHAR, null);
+            
+            /** data */
+            flFld.setStringField(this, __root.getData(this.masked), 10, Constants.ALIGN_NON, " ", 0, true, false, Constants.ENCODE_CHAR, null);
+        }
+        
+        return flFld.getMessage();
+    }
+
+        
+    private void marshallNullObject(FixedLengthMarshalFieldV2 flFld) throws Exception {
+            
+            /** data1 */
+
+                flFld.padByteArrayField(this, 10, " ");
+            
+            /** data */
+
+                flFld.padByteArrayField(this, 10, " ");
+    }
+
+    /* unmarshal method */
+    public Object unmarshalObject(byte[] message, int startOffset) throws Exception{
+        hwgi.o28.mem.txGrp_1000.tx_220.MSG dataObject = new hwgi.o28.mem.txGrp_1000.tx_220.MSG();
+        setOffset(unmarshalObject(message, startOffset, dataObject));
+            return dataObject;
+    }
+    
+    public int unmarshalObject(byte[] message, int startOffset, Object dataObject) throws Exception {        
+
+        hwgi.o28.mem.txGrp_1000.tx_220.MSG __root = (hwgi.o28.mem.txGrp_1000.tx_220.MSG)dataObject;
+        FixedLengthUnmarshalField flFld = new FixedLengthUnmarshalField(message, startOffset);
+        
+        set_data(__root, flFld);
+        set_data1(__root, flFld);
+
+        setOffset(flFld.getOffset());
+        __root.setMessageProperties(fieldPropertyMap);
+        return getOffset();
+    }
+
+    private void set_data(hwgi.o28.mem.txGrp_1000.tx_220.MSG __root, FixedLengthUnmarshalField flFld) throws Exception {
+        try {
+
+        /** data */
+        __root.setData(flFld.getStringField(this, 10, Constants.ALIGN_NON, " ", 0, true, false, Constants.ENCODE_CHAR, Constants.TRIM_NONE), this.masked);
+    successFieldCount++;
+        } catch(Exception e) {
+            StringBuilder esb = new StringBuilder(e.getClass().getName()).append(" [FIELD: data] ");
+            if(e.getMessage() != null)
+                esb.append(": ").append(e.getMessage());
+            throw new Exception(esb.toString(), e);
+        }
+    }
+    private void set_data1(hwgi.o28.mem.txGrp_1000.tx_220.MSG __root, FixedLengthUnmarshalField flFld) throws Exception {
+        try {
+
+        /** data1 */
+        __root.setData1(flFld.getStringField(this, 10, Constants.ALIGN_NON, " ", 0, true, false, Constants.ENCODE_CHAR, Constants.TRIM_NONE), this.masked);
+    successFieldCount++;
+        } catch(Exception e) {
+            StringBuilder esb = new StringBuilder(e.getClass().getName()).append(" [FIELD: data1] ");
+            if(e.getMessage() != null)
+                esb.append(": ").append(e.getMessage());
+            throw new Exception(esb.toString(), e);
+        }
+    }
+    
+    protected static java.util.Map<String, MessageFieldProperty> fieldPropertyMap = new java.util.LinkedHashMap();
+    public java.util.Map<String, MessageFieldProperty> getFieldPropertyMap() {
+        return fieldPropertyMap;
+    }
+    static {
+
+        fieldPropertyMap.put("data", getProperty_data());
+        fieldPropertyMap.put("data1", getProperty_data1());
+    }
+
+    private static MessageFieldProperty getProperty_data() {
+        MessageFieldProperty fldProp = null;
+        fldProp = new MessageFieldProperty("data", "data", MessageFieldType.STRING, (String)null, null, "10", Constants.ALIGN_NON, " ", 0, true, false, null, null, null, null, null, Constants.TRIM_NONE, Constants.ENCODE_CHAR);
+        return fldProp;
+    }
+
+    private static MessageFieldProperty getProperty_data1() {
+        MessageFieldProperty fldProp = null;
+        fldProp = new MessageFieldProperty("data1", "data2", MessageFieldType.STRING, (String)null, null, "10", Constants.ALIGN_NON, " ", 0, true, false, null, null, null, null, null, Constants.TRIM_NONE, Constants.ENCODE_CHAR);
+        return fldProp;
+    }
+
+
+
+
+    /**
+     * @param input 
+     *  values of input variable are 
+     *                                         physicalname
+     *                                        versionno
+     *                                        logicalname
+     *                                        resourceid
+     *                                   resourceType
+     *                                   resourcePath
+     *                                   resourceGroup
+     * @return 
+     */
+    public String getMetaData(String input) {
+
+        if (input.equalsIgnoreCase(META_PHYSICAL_NAME)) {
+            return "MSGFixedLength";
+        } else if (input.equalsIgnoreCase(META_VERSION_NO)) {
+            return null;
+        } else if (input.equalsIgnoreCase(META_LOGICAL_NAME)) {
+            return "MSG";
+        } else if (input.equalsIgnoreCase(META_RESOURCE_ID)) {
+            return "hwgi.o28.mem.txGrp_1000.tx_220:MSGFixedLength.msg";
+        } else if (input.equalsIgnoreCase(META_RESOURCE_TYPE)) {
+            return "MESSAGE";
+        } else if (input.equalsIgnoreCase(META_RESOURCE_PATH)) {
+            return "hwgi.o28.mem.txGrp_1000.tx_220";
+        } else if (input.equalsIgnoreCase(META_RESOURCE_GROUP)) {
+            return "";
+        } else if (input.equalsIgnoreCase(META_MESSAGE_TYPE)) {
+              return "FixedLength";
+        } else if (input.equalsIgnoreCase(META_MESSAGE_STRUCTURE_NAME)) {
+            return "MSG";
+        } else if (input.equalsIgnoreCase(META_MESSAGE_STRUCTURE_PATH)) {
+            return "hwgi.o28.mem.txGrp_1000.tx_220";
+        } else if (input.equalsIgnoreCase(META_MESSAGE_STRUCTURE_ID)) {
+            return "hwgi.o28.mem.txGrp_1000.tx_220:MSG.umsg";
+        } else {
+            String msg = "Expecting one of " + META_PHYSICAL_NAME + ", "
+                    + META_VERSION_NO + ", " + META_LOGICAL_NAME + ", "
+                    + META_RESOURCE_ID + ", " + META_RESOURCE_TYPE + ", "
+                    + META_RESOURCE_PATH + ", " + META_RESOURCE_GROUP + " , "
+                    + META_MESSAGE_TYPE + " , " + META_MESSAGE_STRUCTURE_NAME
+                    + " , " + META_MESSAGE_STRUCTURE_PATH + " , "
+                    + META_MESSAGE_STRUCTURE_ID;
+            
+            throw new IllegalArgumentException(msg);
+        }
+    }        
+}
